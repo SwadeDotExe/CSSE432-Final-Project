5beed3226decc773217799ca503ea0eaae9af8c9
SwadeDotExe <swadecirata@gmail.com>
2023-05-05 08:32:24 -0400

    Added metadata parsing

--- Testing/metadataTesting.py
+++ Testing/metadataTesting.py
@@ -4,5 +4,17 @@ import audio_metadata
 
 metadata = audio_metadata.load('FinalProject/AudioFiles/JC Overture.mp3')
 
-# Print metadata
-print(metadata)
+# Print all metadata
+# print(metadata)
+
+# Save metadata to variables
+artist = metadata['tags']['artist'][0]
+album = metadata['tags']['album'][0]
+title = metadata['tags']['title'][0]
+duration = metadata['streaminfo']['duration']
+
+# Print metadata of interest
+print(artist)
+print(album)
+print(title)
+print(duration)
